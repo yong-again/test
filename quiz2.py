d4938533f3b564be93e525d8792d4f3fc2ab1ece
yong-again <musehead91@gmail.com>
2022-11-30 15:52:15 +0900

    add quiz

--- quiz2.py
+++ quiz2.py
@@ -5,15 +5,12 @@ print('::'.join(map(str, numbers)))
 
 # Q2.
 numbers = list(range(1, 10 + 1))
-
 print("# 홀수만 추출하기")
-print(list(filter(lambda n: n % 3==0, numbers)))
+print(list(filter(lambda n: n % 2 == 1, numbers)))
 
 print("# 3이상, 7 미만 추출하기")
-print(list(filter(lambda n: n >=3 and n <7, numbers)))
+print(list(filter(lambda n: n >=3 and n < 7, numbers)))
 
 print("# 제곱해서 50 미만 추출하기")
-def underfifthy(n):
-    return n < 50
-print(list(filter(underfifthy, map(lambda n: n ** 2, numbers))))
+print(list(filter(lambda n: n ** 2 < 50, numbers)))
 
